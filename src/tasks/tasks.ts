d4aca3649d3fae1f9f9e429fee47082cadda76b3
cryptickoan <me@cryptickoan.com>
2022-02-13 16:31:04 -0600

    refactor

--- src/tasks/tasks.ts
+++ src/tasks/tasks.ts
@@ -1,6 +1,6 @@
 import '@nomiclabs/hardhat-ethers';
 import { task } from 'hardhat/config';
-import { FuseDeployment } from '../fuse-deployer';
+import { FuseDeployment } from '../fuse/deployer';
 
 task('deploys', 'deploys a clean fuse instance', async (taskArgs, hre) => {
         const [deployer] = await hre.ethers.getSigners();
