e03099f750c248bb748a89dd1c2944acf89bf9ef
cryptickoan <me@cryptickoan.com>
2022-02-13 16:28:02 -0600

    Importing task to hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,26 +1,6 @@
-import * as dotenv from "dotenv";
-
-import { HardhatUserConfig, task } from "hardhat/config";
-import "@nomiclabs/hardhat-etherscan";
-import "@nomiclabs/hardhat-waffle";
-import "@typechain/hardhat";
-import "hardhat-gas-reporter";
-import "solidity-coverage";
-
-dotenv.config();
-
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
+import { HardhatUserConfig } from "hardhat/config";
+import '@nomiclabs/hardhat-ethers';
+import './src/tasks/tasks'
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -36,7 +16,7 @@ const config: HardhatUserConfig = {
   networks: {
     hardhat: {
       forking: {
-        url: "https://eth-mainnet.alchemyapi.io/v2/I4TZi8QLjwehpJbFplMHTXjNkQVbqwA-",
+        url: "https://eth-mainnet.alchemyapi.io/v2/2Mt-6brbJvTA4w9cpiDtnbTo6qOoySnN",
         blockNumber: 14167154
       },
       allowUnlimitedContractSize: true,
