b58b4565906528c4db305b60f585567c72a2fa01
cryptickoan <me@cryptickoan.com>
2022-02-13 21:13:13 -0600

    Created getPoolInfo function

---
+++ src/fuse/info/get-pool-info.ts
@@ -0,0 +1,31 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types"
+import { Fuse } from "../../../cjs"
+
+/**
+ * @param hre Hardhat runtime environment, passed from task.
+ * @param comptroller the pool's comptroller address.
+ * @param address the user's address - used to calculate user's balance in listed markets.
+ * @param fuse an initiated fuse instance.
+ * @returns an array with listed markets and their information.
+ */
+export async function getPoolInfo(
+    hre: HardhatRuntimeEnvironment,
+    comptroller: string,
+    address: string,
+    fuse: Fuse
+  ) {
+    const comptrollerContract = new hre.ethers.Contract(
+        comptroller,
+        JSON.parse(
+                fuse.compoundContracts["contracts/Comptroller.sol:Comptroller"].abi
+              ),
+        fuse.provider
+    )   
+
+    const data = await fuse.contracts.FusePoolLens.callStatic.getPoolAssetsWithData(
+            comptroller,
+            { from: address }
+        )
+
+    return data
+  }
