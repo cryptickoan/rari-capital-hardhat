cbf3c23858cd75ccc4e4c5d34a6c87eab3fdfe59
cryptickoan <me@cryptickoan.com>
2022-02-16 02:08:00 -0600

    Update params - better dev exp

--- src/fuse/deploy/deploy-market.ts
+++ src/fuse/deploy/deploy-market.ts
@@ -10,7 +10,9 @@ import { Fuse } from '../../../cjs/index';
  * @param comptrollerAddress - The comptroller's address.
  * @param address - The user's address, has to be comptroller admin.
  * @param underlyingAsset - The underlying asset's address. i.e "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48" for USDC.
- * 
+ * @param collateralFactor - Determines loan to value ratio. i.e. how much of collateral value can be borrowed. Should be between 0 - 1.
+ * @param reserveFactor - Determines percentage for reserves. Should be between 0 - 1.
+ * @param adminFee - Determines percentage for admin fees. Should be between 0 - 1.
  */
 export async function deployMarket(
   fuse: Fuse,
