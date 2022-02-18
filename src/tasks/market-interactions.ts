9ad76f02193ff5a9b56ae7ca33545874a6ec1149
cryptickoan <me@cryptickoan.com>
2022-02-18 13:57:38 -0600

    :bug: Fix

--- src/tasks/market-interactions.ts
+++ src/tasks/market-interactions.ts
@@ -1,5 +1,4 @@
 import '@nomiclabs/hardhat-ethers';
-import { BigNumber } from 'ethers';
 import { task } from 'hardhat/config';
 import { supplyCEther } from '../fuse/comptroller-interactions/supply/cEth';
 
@@ -13,35 +12,35 @@ task('supply', 'Will supply amount of token to market.')
     .addParam('underlying', 'Address for the underlying token of the market to supply to. 0 if its ether.')
     .addParam('market', 'Address of the market/ctoken to supply to.')
     .addParam('comptroller', 'Address of comptroller where the market is listed on.')
-    .addParam('amunt', 'Amount of underlying to supply.')
-    .addOptionalParam('enableAsCollateral', 'If true, the supplied amount will be enabled as collateral.')
+    .addParam('amount', 'Amount of underlying to supply.')
+    .addOptionalParam('collateralize', 'If true, the supplied amount will be enabled as collateral.')
     .addOptionalParam('user', 'Address of user that is supplying. Must be a hardhat address.')
     .setAction( async (taskArgs, hre) => {
         const {address, fuse, fuseDeployed} = await configureEnv(hre)
         if (!fuseDeployed) return
 
-        const userAddress = taskArgs.address ?? address
+        const userAddress = taskArgs.user ?? address
 
-        if (taskArgs.underlying !== 0 ){
+        if (taskArgs.underlying !== '0'){
             await supplyCToken(
                 fuse,
                 fuse.provider,
                 userAddress,
-                taskArgs.tokenAddress,
-                taskArgs.marketAddress,
+                taskArgs.underlying,
+                taskArgs.market,
                 taskArgs.amount,
-                taskArgs.enableAsCollateral,
-                taskArgs.comptrollerAddress
+                taskArgs.collateralize ?? false,
+                taskArgs.comptroller
             )
         } else {
             await supplyCEther(
                 fuse,
                 fuse.provider,
                 userAddress,
-                taskArgs.marketAddress,
+                taskArgs.market,
                 taskArgs.amount,
-                taskArgs.enableAsCollateral,
-                taskArgs.comptrollerAddress
+                taskArgs.collateralize ?? false,
+                taskArgs.comptroller
             )
         }
     }
